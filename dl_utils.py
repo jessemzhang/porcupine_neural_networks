27fe838f66c6004e94f9f08778b651951de62042
Jesse Zhang <jessez@stanford.edu>
2018-04-20 13:34:23 -0700

    cleaned up dl_utils a bit, added a training function for no wtrue (good for real data), initial results on mnist

--- dl_utils.py
+++ dl_utils.py
@@ -46,13 +46,15 @@ def compute_wgap_and_loss(X,Y,w_true,graph,sess,batch_size=100):
 
 # w_initial should be a dictionary with keys corresponding to weight matrices in the graph
 def train(X,Y,graph,num_epochs,batch_size,w_true,w_initial=None,verbose=True,savedir=None,
-          early_stop_loss=1e-5,lr_initial=0.01,get_update_history=False):
+          early_stop_loss=1e-5,lr_initial=0.01,get_update_history=False, seed=0):
 
     if verbose: start = time.time()
     training_losses = []
     training_w_gaps = []
     if get_update_history: weights_history = []
-    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+    tf.set_random_seed(seed)
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                                          gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=gpu_prop))) as sess:
         sess.run(tf.global_variables_initializer())
         if w_initial is not None: 
             for k in w_initial: sess.run(graph[k].assign(w_initial[k]))
@@ -69,6 +71,7 @@ def train(X,Y,graph,num_epochs,batch_size,w_true,w_initial=None,verbose=True,sav
             training_w_gap = 0
             steps = 0.
             X_, Y_ = shuffle(X,Y)
+            
             for i in range(0,len(X),batch_size):
                 x,y = X_[i:i+batch_size], Y_[i:i+batch_size]
                 feed_dict = {graph['w'][k]:w_true[k] for k in w_true}
@@ -126,19 +129,93 @@ def train(X,Y,graph,num_epochs,batch_size,w_true,w_initial=None,verbose=True,sav
            initial_train_loss,initial_train_w_gap,final_train_loss,final_w_gap
 
 
+# w_initial should be a dictionary with keys corresponding to weight matrices in the graph
+def train_no_wtrue(X, Y, graph, num_epochs, batch_size, 
+                   w_initial=None, verbose=True,savedir=None, early_stop_loss=1e-5,
+                   lr_initial=0.01, get_update_history=False, gpu_prop=0.1, seed=0):
+
+    if verbose: start = time.time()
+    training_losses = []
+    training_w_gaps = []
+    if get_update_history: weights_history = []
+    tf.set_random_seed(seed)
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                                          gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=gpu_prop))) as sess:
+        
+        sess.run(tf.global_variables_initializer())
+        
+        if w_initial is not None: 
+            for k in w_initial: sess.run(graph[k].assign(w_initial[k]))
+        
+        for epoch in range(num_epochs):
+            lr = lr_initial*0.95**(epoch/390.) # initial lr * decay rate ^(step/decay_steps)
+            sess.run(graph['learning_rate'].assign(lr))
+            t = time.time()
+            training_loss = 0
+
+            steps = 0.
+            X_, Y_ = shuffle(X,Y)
+            for i in range(0,len(X),batch_size):
+                x, y = X_[i:i+batch_size], Y_[i:i+batch_size]
+                
+                feed_dict = {graph['x']: x, graph['y']: y}
+                if get_update_history:
+                    training_loss_, _, grads, mgrads, w = sess.run([graph['total_loss'],
+                                                                    graph['opt_step'],
+                                                                    graph['grads'],
+                                                                    graph['modified_grads'],
+                                                                    graph['weights1']],
+                                                                   feed_dict=feed_dict)
+                    weights_history.append((grads, mgrads, w))
+
+                else:
+                    training_loss_, _ = sess.run([graph['total_loss'], graph['opt_step']],
+                                                 feed_dict=feed_dict)
+                
+                training_loss += training_loss_
+                steps += 1.
+                
+                if verbose:
+                    print('\rEpoch %s/%s (%.3f s), batch %s/%s (%.3f s): loss %.3f'
+                          %(epoch+1, num_epochs, time.time()-start, steps,
+                            len(X_)/batch_size, time.time()-t, training_loss_), end='')
+
+            training_losses.append(training_loss/steps)
+
+            if early_stop_loss is not None and np.mean(training_losses[-10:]) <= early_stop_loss:
+                if verbose:
+                    print('\rMean loss <= %.3e for last 10 epochs. Stopping training after epoch %s/%s.'
+                          %(early_stop_loss,epoch+1,num_epochs+1),end='')
+                break
+
+        w_hat = {k:sess.run(graph[k]) for k in graph if 'weights' in k} # grab all final weights
+        
+        # only saving weights from last epoch
+        if savedir is not None: 
+            os.system('mkdir -p %s'%(savedir))
+            graph['saver'].save(sess,'%sepoch%s'%(savedir,epoch))
+            
+    if verbose: print('')
+    if get_update_history:
+        return training_losses, w_hat, weights_history
+    
+    return training_losses, w_hat
+
+
 # ------------------------------------------------------------------------------
 # Computing useful values from the network
 
-def compute_w_gap(w1,w2):
+def compute_w_gap(w1, w2):
     return np.sum([np.linalg.norm(w1[k]-w2[k])**2 for k in w1])
 
 # Generate random labels with underlying true weights
-def generate_random_weights_and_output(X,input_dict,build_func=arch.mlp,batch_size=100,seed=0):    
+def generate_random_weights_and_output(X, input_dict, build_func=arch.mlp, batch_size=100, seed=0, gpu_prop=0.1):    
     tf.reset_default_graph()
     tf.set_random_seed(seed)
     graph = build_func(input_dict)
     y = np.zeros((len(X),1))
-    with tf.Session() as sess:
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                                          gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=gpu_prop))) as sess:
         sess.run(tf.global_variables_initializer())
         w = {k:sess.run(graph[k]) for k in graph if 'weights' in k} # grab all weights for graph
         for i in range(0,len(X),batch_size):
@@ -147,11 +224,12 @@ def generate_random_weights_and_output(X,input_dict,build_func=arch.mlp,batch_si
     return y,w
 
 # Generate labels with underlying true weights
-def generate_output(X,w,input_dict,build_func=arch.mlp,batch_size=100):    
+def generate_output(X, w, input_dict, build_func=arch.mlp, batch_size=100, gpu_prop=0.1):
     tf.reset_default_graph()
     graph = build_func(input_dict)
     y = np.zeros((len(X),1))
-    with tf.Session() as sess:
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                                          gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=gpu_prop))) as sess:
         sess.run(tf.global_variables_initializer())
         for k in w: sess.run(graph[k].assign(w[k]))
         for i in range(0,len(X),batch_size):
@@ -160,11 +238,12 @@ def generate_output(X,w,input_dict,build_func=arch.mlp,batch_size=100):
     return y
 
 # Generate loss for some given weights w
-def generate_loss(X,Y,w,input_dict,build_func=arch.mlp,batch_size=100):
+def generate_loss(X, Y, w, input_dict, build_func=arch.mlp, batch_size=100, gpu_prop=0.1):
     tf.reset_default_graph()
     graph = graph_builder_wrapper(input_dict,build_func=build_func)
     L = []
-    with tf.Session() as sess:
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                                          gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=gpu_prop))) as sess:
         sess.run(tf.global_variables_initializer())
         for k in w: sess.run(graph[k].assign(w[k]))
         for i in range(0,len(X),batch_size):
@@ -173,7 +252,7 @@ def generate_loss(X,Y,w,input_dict,build_func=arch.mlp,batch_size=100):
     return np.mean(L)
 
 # Compute loss for multiple combinations of w1, w2
-def compute_L(X,Y,all_wi,all_wj,input_dict,build_func=arch.mlp_simple):
+def compute_L(X, Y, all_wi, all_wj, input_dict, build_func=arch.mlp_simple):
     n = len(all_wi)
     L = np.zeros((n,n))
     start = time.time()
@@ -186,11 +265,12 @@ def compute_L(X,Y,all_wi,all_wj,input_dict,build_func=arch.mlp_simple):
 
 # Get hidden node states (either on or off) using given weights
 # weights is a dictionary with keys corresponding to weight matrices in the graph
-def get_hidden_states(X,input_dict,weights,build_func=arch.mlp,batch_size=100):
+def get_hidden_states(X, input_dict, weights, build_func=arch.mlp, batch_size=100, gpu_prop=0.1):
     tf.reset_default_graph()
     graph = build_func(input_dict)
     states = []
-    with tf.Session() as sess:
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                                          gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=gpu_prop))) as sess:
         sess.run(tf.global_variables_initializer())
         for i in range(0,len(X),batch_size):
             feed_dict = {graph[k]:weights[k] for k in weights}
@@ -202,10 +282,11 @@ def get_hidden_states(X,input_dict,weights,build_func=arch.mlp,batch_size=100):
 
 
 # Get everything the train function would return
-def get_train_out(X,Y,w,input_dict,savedir,build_func):
+def get_train_out(X, Y, w, input_dict, savedir, build_func, gpu_prop=0.1):
     tf.reset_default_graph()
     graph = graph_builder_wrapper(input_dict,build_func=build_func)
-    with tf.Session() as sess:
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                                          gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=gpu_prop))) as sess:
         epoch = os.listdir(savedir)[0].split('.')[0].split('epoch')[1]
         graph['saver'].restore(sess,'%sepoch%s'%(savedir,epoch))
         w_hat = {k:sess.run(graph[k]) for k in graph if 'weights' in k}
